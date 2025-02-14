da82256dbb450f2e791a73e13f6fc20904293a49
Matt Henchey <matthew.henchey@gmail.com>
2025-02-14 13:49:14 -0500

    update vite.config.ts and +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import '../app.css';
+	import '$lib/styles/reset.css';
 </script>
 
 <div
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { svelteTesting } from '@testing-library/svelte/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-
+	css: {
+		preprocessorOptions: {
+			scss: {
+				additionalData: ''
+			}
+		}
+	},
 	test: {
 		workspace: [
 			{
