8351d26b0d7d05efce1361486bfa84bb51f31f5f
Matt Henchey <matthew.henchey@gmail.com>
2025-02-16 15:38:31 -0500

    fixing mistake in About section

--- src/lib/data/about.ts
+++ src/lib/data/about.ts
@@ -1,6 +1,6 @@
 export const aboutData = {
     description: [
-        "With over ten years of software engineering experience, I’ve developed a strong background in back-end development, specializing in system architecture and data processing. I focus on building high-performance systems with Golang while also exploring front-end development with modern frameworks such as TypeScript, Svelte, and React.",
+        "With over ten years of software engineering experience, I’ve developed a strong background in back-end development, specializing in system architecture and data processing. I focus on building high-performance systems with Golang and Typescript while also exploring front-end development with modern frameworks such as Svelte and React.",
         "Transitioning from traditional photography to software engineering has been a journey fueled by curiosity and a passion for mastering complex processes. The same drive that led me to experiment with 18th-century photographic techniques now inspires my approach to development: embracing challenges, thinking creatively, and continually expanding my skill set.",
         "My colleagues describe me as a natural self-starter who is hungry to learn as much as possible. Through dedication, long hours, and the generous mentorship of my peers, I've rapidly evolved from writing one-off tools to developing production-ready applications, demonstrating my commitment to continuous learning and professional growth."
     ],
