3425f49bda413eb1754641ea9ea25b710701b2c0
Matt Henchey <matthew.henchey@gmail.com>
2025-02-16 20:48:37 -0500

    adjust Header to improve quote display

--- src/lib/components/layout/Header.svelte
+++ src/lib/components/layout/Header.svelte
@@ -12,20 +12,20 @@
 	<div class="backdrop"></div>
 	<!-- Quote Section -->
 	<div
-		class="max-w-8xl text-center opacity-0 transition-all duration-300 h-full relative"
+		class="max-w-4xl mx-auto text-center opacity-0 transition-all duration-300 h-full relative"
 		class:opacity-100={isVisible}
 	>
 		<p
-			class="text-sm md:text-lg font-medium bg-gradient-to-r from-green-600 to-green-900 dark:from-green-600 dark:to-green-900 text-transparent bg-clip-text px-4"
+			class="text-sm md:text-lg font-medium bg-gradient-to-r from-green-600 to-green-900 dark:from-green-600 dark:to-green-900 text-transparent bg-clip-text px-4 py-2"
 		>
 			{currentQuote.quote}
 		</p>
-		<p class="text-sm md:text-md font-light text-green-900 dark:text-green-200 italic">
+		<p class="text-xs md:text-md font-light text-green-900 dark:text-green-200 italic">
 			{currentQuote.author}
 		</p>
 	</div>
 	<!-- Theme Toggle -->
-	<div class="absolute top-4 right-4">
+	<div class="absolute bottom-2 right-2">
 		<slot name="theme-toggle" />
 	</div>
 </header>
@@ -36,7 +36,7 @@
 		position: sticky;
 		top: 0;
 		width: 100%;
-		height: 5rem;
+		height: 6rem;
 	}
 	@supports (backdrop-filter: blur(16px)) or (-webkit-backdrop-filter: blur(16px)) {
 		/* DARK MODE (if .dark is present on the root) */
